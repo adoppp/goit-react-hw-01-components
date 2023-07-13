44d1673ff4598dd0374dc21d3ce44315ef57ec42
adoppp <121878757+adoppp@users.noreply.github.com>
2023-07-13 17:51:59 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,46 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile/>
     </div>
   );
 };
+
+import user from './user.json';
+
+
+const Profile = ({ username, tag, location, avatar, stats }) => (
+  <div className="profile">
+    <div className="description">
+      <img src={avatar} alt="User avatar" className="avatar" />
+      <p class="name">{username}</p>
+      <p class="tag">{tag}</p>
+      <p class="location">{location}</p>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+const PropsForProfile = () => (<div>
+  <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+  />
+</div>);
